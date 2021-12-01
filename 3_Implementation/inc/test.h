ed44fd138d36ae174d27af512849752c0fbe96c0
iswaryadhanapalan <94157594+iswaryadhanapalan@users.noreply.github.com>
2021-12-01 15:24:21 +0530

    Update test.h

--- 3_Implementation/inc/test.h
+++ 3_Implementation/inc/test.h
@@ -1,1 +1,19 @@
+#define F_CPU 16000000UL
 
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <string.h>
+#include <util/delay.h>
+
+#define keypadDirectionRegisterR DDRB
+#define keypadPortControlR PORTB
+#define keypadPortValueR PINB
+
+#define keypadDirectionRegisterC DDRC
+#define keypadPortControlC PORTC
+#define keypadPortValueC PINC
+
+#define ctrl PORTD // We are using port D
+#define en 2       // enable signal pin 2
+#define rw 1       // read/write signal pin 1
+#define rs 0       // register select signal pin 0
